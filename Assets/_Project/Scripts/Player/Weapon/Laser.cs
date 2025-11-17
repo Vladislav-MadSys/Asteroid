e94d59131c153e52da702c00d3dbf96c702b54f1
VlaMor60305 <73854090+VlaMor60305@users.noreply.github.com>
2025-11-17 03:43:12 +0300

    Laser

---
+++ Assets/_Project/Scripts/Player/Weapon/Laser.cs
@@ -0,0 +1,109 @@
+ using UnityEngine;
+using Zenject;
+using Zenject.SpaceFighter;
+
+public class Laser : MonoBehaviour
+{
+    private Transform _transform;
+    private PlayerInputHandler _playerInputHandler;
+
+    [SerializeField] private float range = 10;
+    [SerializeField] private LineRenderer _lineRenderer;
+    [SerializeField] private float _maxCharges = 3;
+    private float _curCharges = 0;
+
+    //Fire
+    [SerializeField] private float _shootingDelay = 2f;
+    private float _shootingTimer;
+
+
+    //Charges
+    [SerializeField] private float _chargeDelay = 5f;
+    private float _chargeTimer;
+
+    private bool _canFire = false;
+
+    
+
+    [Inject]
+    void Inject(PlayerInputHandler playerInputHandler)
+    {
+        _playerInputHandler = playerInputHandler;
+    }
+    private void Awake()
+    {
+        _transform = transform;
+    }
+    private void Update()
+    {
+        
+
+        //Shooting
+        if (_canFire)
+        {
+            if (_curCharges > 0)
+            {
+                if (_playerInputHandler.isFire2ButtonPressed)
+                {
+                    Fire();
+                    _curCharges -= 1;
+                    _canFire = false;
+                    _shootingTimer = 0;
+                }
+            }
+        }
+        else
+        {
+            if (_shootingTimer >= _shootingDelay)
+            {
+                _canFire = true;
+                if (_lineRenderer != null)
+                {
+                    _lineRenderer.SetPosition(0, _transform.position);
+                    _lineRenderer.SetPosition(1, _transform.position);
+                }
+            }
+            else
+            {
+                _shootingTimer += Time.deltaTime;
+            }
+        }
+
+        //Charges
+        if(_curCharges < _maxCharges)
+        {
+            if(_chargeTimer >= _chargeDelay)
+            {
+                _curCharges++;
+                _chargeTimer = 0;
+            }
+            else
+            {
+                _chargeTimer += Time.deltaTime;
+            }
+        }
+
+    }
+
+    void Fire()
+    {
+        Vector2 startPoint = transform.position;
+        Vector2 endPoint = startPoint + (Vector2)_transform.up * range;
+
+        if (_lineRenderer != null)
+        {
+            _lineRenderer.SetPosition(0, startPoint);
+            _lineRenderer.SetPosition(1, endPoint);
+        }
+
+        RaycastHit2D[] hits = Physics2D.LinecastAll(startPoint, endPoint);
+
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider.TryGetComponent<Enemy>(out Enemy enemy))
+            {
+                enemy.Kill();
+            }
+        }
+    }
+}
