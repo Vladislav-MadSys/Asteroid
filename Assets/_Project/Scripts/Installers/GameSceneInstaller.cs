571390daf404cc093a63658a2e51053ef9e9dbf6
VlaMor60305 <vlades60305@gmail.com>
2026-01-12 02:59:03 +0300

    SAVE SYSTEM Player and UI instantiated at runtime

--- Assets/_Project/Scripts/Installers/GameSceneInstaller.cs
+++ Assets/_Project/Scripts/Installers/GameSceneInstaller.cs
@@ -13,22 +13,24 @@ namespace _Project.Scripts.Installers
     public class GameSceneInstaller : MonoInstaller
     {
         [SerializeField] private SpawnerSettings[] spawnerSettings;
+        [SerializeField] private GameObject _playerShipPrefab;
+        [SerializeField] private GameObject _playerStatsHudViewPrefab;
         
         public override void InstallBindings()
         {
+            Container.Bind<SceneController>().AsSingle();
             Container.Bind<PlayerStates>().AsSingle();
             Container.Bind<PlayerInput>().AsSingle();
-            Container.BindInterfacesAndSelfTo<Portal>().AsSingle();
-            Container.Bind<EnemyDeathListener>().AsSingle();
-            Container.BindInterfacesAndSelfTo<SpawnersManager>().AsSingle().NonLazy();
-            Container.Bind<Camera>().FromInstance(Camera.main).AsSingle().NonLazy();
-            Container.Bind<SceneController>().AsSingle();
             Container.BindInterfacesAndSelfTo<PlayerInputHandler>().AsSingle();
-            Container.Bind<PlayerShip>().FromComponentInHierarchy().AsSingle();
+            Container.Bind<PlayerShip>().FromComponentInNewPrefab(_playerShipPrefab).AsSingle();
+            Container.Bind<Camera>().FromInstance(Camera.main).AsSingle().NonLazy();
+            Container.BindInterfacesAndSelfTo<Portal>().AsSingle();
+            Container.BindInterfacesAndSelfTo<SpawnersManager>().AsSingle().NonLazy();
             Container.Bind<SpawnerSettings[]>().FromInstance(spawnerSettings).AsSingle();
+            Container.Bind<EnemyDeathListener>().AsSingle();
             Container.BindInterfacesAndSelfTo<PlayerStatsHudPresenter>().AsSingle().NonLazy();
             Container.BindInterfacesAndSelfTo<PlayerStatsHudModel>().AsSingle().NonLazy();
-            Container.Bind<PlayerStatsHudView>().FromComponentInHierarchy().AsSingle();
+            Container.Bind<PlayerStatsHudView>().FromComponentInNewPrefab(_playerStatsHudViewPrefab).AsSingle();
             Container.BindInterfacesAndSelfTo<GameSessionData>().AsSingle().NonLazy();
             Container.BindInterfacesAndSelfTo<SceneSaveController>().AsSingle().NonLazy();
         }
